61285b7813fae6ae5fe7f50163533724615b2361
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2023-01-11 09:45:40 -0300

    correcao no pom.xml

---
+++ src/main/java/com/dev/agendalive/repository/LiveRepository.java
@@ -0,0 +1,14 @@
+package com.dev.agendalive.repository;
+
+import com.dev.agendalive.document.LiveDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+@Repository
+public interface LiveRepository extends MongoRepository<LiveDocument, String> {
+    Page<LiveDocument> findByLiveDateAfterOrderByLiveDateAsc(LocalDateTime date, Pageable pageable);
+    Page<LiveDocument> findByLiveDateBeforeOrderByLiveDateDesc(LocalDateTime date, Pageable pageable);
+}
